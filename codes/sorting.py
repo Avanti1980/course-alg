90e0a861a5fd30d236a92d6442b8186173b1b893
murongxixi <murongxixi@gmail.com>
2022-10-25 04:17:06 +0800

    update

--- codes/sorting.py
+++ codes/sorting.py
@@ -1,5 +1,4 @@
 def bubble_sort(a, n):
-    t = 0
     for i in range(n-1):
         for j in range(n-1, i, -1):
             if a[j] < a[j-1]:
@@ -7,24 +6,34 @@ def bubble_sort(a, n):
 
 
 def selection_sort(a, n):
-    for i in range(n):
-        min_index = i
-        min_val = a[i]
+    for i in range(n-1):
+        smallest = i
         for j in range(i+1, n):
-            if a[j] < min_val:
-                min_val = a[j]
-                min_index = j
-        a[i], a[min_index] = a[min_index], a[i]
+            if a[j] < a[smallest]:
+                smallest = j
+        a[i], a[smallest] = a[smallest], a[i]
+
+
+def insertion_sort(a, n):
+    for i in range(1, n):
+        key = a[i]
+        j = i - 1
+        while j >= 0 and key < a[j]:
+            a[j+1] = a[j]
+            j -= 1
+        a[j+1] = key
 
 
 a = [5, 2, 4, 6, 1, 3]
 
-if len(a) > 1:
+b = [5, 2, 4, 6, 1, 3]
+bubble_sort(b, len(b))
+print(b)
 
-    b = a
-    bubble_sort(b, len(b))
-    print(b)
+b = [5, 2, 4, 6, 1, 3]
+selection_sort(b, len(b))
+print(b)
 
-    b = a
-    selection_sort(b, len(b))
-    print(b)
+b = [5, 2, 4, 6, 1, 3]
+insertion_sort(b, len(b))
+print(b)
