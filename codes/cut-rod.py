0b5563f64e09ab76b4addb324e3cbf6b54c88bab
murongxixi <murongxixi@gmail.com>
2022-11-10 02:52:16 +0800

    update

--- codes/cut-rod.py
+++ codes/cut-rod.py
@@ -1,36 +1,76 @@
-def qiege_digui1(p, n):  # 双递归
-    if n == 0:
-        return 0
+from math import ceil  # 向上取整
+
+
+def cut_rod_rec(n):  # 单子问题
+    if n == 1:
+        return p[1]
     else:
-        res = p[n]  # 取不切割和切割之间的最大值
-        for i in range(1, n):  # 切割的价格由两段被分割的部分分别递归得到
-            res = max(res, qiege_digui1(p, n-i)+qiege_digui1(p, i))
-    return res
+        v = p[n]
+        for i in range(1, n):
+            v = max(v, p[i] + cut_rod_rec(n-i))
+    return v
 
 
-def qiege_digui2(p, n):  # 单递归
-    if n == 0:
-        return 0
+def cut_rod_rec2(n):  # 双子问题
+    if n == 1:
+        return p[1]
     else:
-        res = p[n]
-        for i in range(1, n):  # 切割的都有一个最小量级，始终可视为切两半之后，一半保持不变
-            res = max(res, p[i]+qiege_digui2(p, n-i))  # 另一半继续递归价格
-    return res
+        v = p[n]
+        for i in range(1, ceil(n/2)):
+            v = max(v, cut_rod_rec2(i) + cut_rod_rec2(n-i))
+    return v
 
 
-def cut_rod_dp(p, n):  # 函数返回：切割长度为 n 的钢条所得的最大收益
+def cut_rod_dp_memoized(n):
+    r = [-infinity] * len(p)  # 初始化为负无穷
+    return cut_rod_dp_memoized_aux(n, r)
+
+
+def cut_rod_dp_memoized_aux(n, r):
+    if r[n] >= 0:  # 查表 若之前已计算过就直接用
+        return r[n]
     if n == 0:
-        return 0
-    q = -1
-    for i in range(1, n+1):
-        q = max(q, p[i] + CutRod(p, n-i))
-        '''
-        tmp = p[i] + CutRod(p, n-i)
-        if q < tmp:
-            q = tmp
-        '''
-    return q
+        v = 0
+    else:
+        v = -infinity
+        for i in range(1, n+1):
+            v = max(v, p[i] + cut_rod_dp_memoized_aux(n-i, r))
+    r[n] = v  # 保存当前计算的结果
+    return v
 
 
-p = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]   # 价格表，下标为对应的钢条长度，如当钢条长度为0时，p=0,即p[0]=0，p[2]=5
-print("最大收益为：", CutRod(p, 7))  # 最大收益为： 10
+def cut_rod_dp_bottom_up(n):
+    r = [0] * len(p)
+    for j in range(1, n+1):  # 依次求解 r[1], r[2], ...
+        v = -infinity
+        for i in range(1, j+1):  # 求解 r[j] 时 遍历 i = 1, 2, ..., j
+            v = max(v, p[i] + r[j-i])  # 此时 r[1], r[2], ..., r[j-1] 均已求好
+        r[j] = v
+    return r[n]
+
+
+def cut_rod_dp_bottom_up_print_sol(n):
+    r = [0] * len(p)
+    s = [0] * len(p)  # s[i]是长度为i的钢条的第一刀最优切割位置
+    for j in range(1, n+1):
+        v = -infinity
+        for i in range(1, j+1):
+            if v < p[i] + r[j-i]:
+                v = p[i] + r[j-i]
+                s[j] = i  # 更新最优切割位置
+        r[j] = v
+    m = n
+    while m > 0:  # 打印最优切割方案
+        print(s[m])
+        m = m - s[m]
+    return r[n]
+
+
+p = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]   # 价格表
+infinity = 1e+16
+
+print(cut_rod_rec(7))
+print(cut_rod_rec2(7))
+print(cut_rod_dp_memoized(7))
+print(cut_rod_dp_bottom_up(7))
+print(cut_rod_dp_bottom_up_print_sol(7))
--- slides/test.md
+++ slides/test.md
@@ -23,5 +23,21 @@ presentation:
 @import "../plugin/chalkboard/style.css"
 @import "../plugin/menu/menu.js"
 
+<!-- slide data-notes="" -->
 
+##### 矩阵连乘问题
 
+---
+
+设$\Av \in \Rbb^{p \times q}$、$\Bv \in \Rbb^{q \times r}$，则$\Cv = \Av \Bv \in \Rbb^{p \times r}$
+
+计算$c_{ij} = \sum_{1 \le k \le q} a_{ik} b_{kj}$需做$q$次标量乘法
+
+计算$\Cv$需$pqr$次标量乘法
+
+<div class="top4"></div>
+
+设$\Av_1 \in \Rbb^{10 \times 100}$、$\Av_2 \in \Rbb^{100 \times 5}$、$\Av_3 \in \Rbb^{5 \times 50}$，根据结合律
+
+- 共$(\Av_1 \Av_2) \Av_3 = 10 \times 100 \times 5 \times 5$次标量乘法
+- 共$\Av_1 (\Av_2 \cdots \Av_3 =---------10 \times 100 \times 5 \times 5$次标量乘法
