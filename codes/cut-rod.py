1859741417cdb41a20b57f04c376537dd6522fda
murongxixi <murongxixi@gmail.com>
2022-11-11 01:56:55 +0800

    update

--- codes/cut-rod.py
+++ codes/cut-rod.py
@@ -23,7 +23,7 @@ def cut_rod_rec2(n):  # 双子问题
 
 
 def cut_rod_dp_memoized(n):
-    r = [-infinity] * len(p)  # 初始化为负无穷
+    r = [-sys.maxsize] * len(p)  # 初始化为负无穷
     return cut_rod_dp_memoized_aux(n, r)
 
 
@@ -33,7 +33,7 @@ def cut_rod_dp_memoized_aux(n, r):
     if n == 0:
         v = 0
     else:
-        v = -infinity
+        v = -sys.maxsize
         for i in range(1, n+1):
             v = max(v, p[i] + cut_rod_dp_memoized_aux(n-i, r))
     r[n] = v  # 保存当前计算的结果
@@ -43,7 +43,7 @@ def cut_rod_dp_memoized_aux(n, r):
 def cut_rod_dp_bottom_up(n):
     r = [0] * len(p)
     for j in range(1, n+1):  # 依次求解 r[1], r[2], ...
-        v = -infinity
+        v = -sys.maxsize
         for i in range(1, j+1):  # 求解 r[j] 时 遍历 i = 1, 2, ..., j
             v = max(v, p[i] + r[j-i])  # 此时 r[1], r[2], ..., r[j-1] 均已求好
         r[j] = v
@@ -54,24 +54,20 @@ def cut_rod_dp_bottom_up_print_sol(n):
     r = [0] * len(p)
     s = [0] * len(p)  # s[i]是长度为i的钢条的第一刀最优切割位置
     for j in range(1, n+1):
-        v = -infinity
+        v = -sys.maxsize
         for i in range(1, j+1):
             if v < p[i] + r[j-i]:
                 v = p[i] + r[j-i]
                 s[j] = i  # 更新最优切割位置
         r[j] = v
-    m = n
-    while m > 0:  # 打印最优切割方案
-        print(s[m])
-        m = m - s[m]
-    return r[n]
+    while n > 0:  # 打印最优切割方案
+        print(s[n], end=' ')
+        n = n - s[n]
 
 
 p = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]   # 价格表
-infinity = sys.maxsize
-
 print(cut_rod_rec(7))
 print(cut_rod_rec2(7))
 print(cut_rod_dp_memoized(7))
 print(cut_rod_dp_bottom_up(7))
-print(cut_rod_dp_bottom_up_print_sol(7))
+cut_rod_dp_bottom_up_print_sol(7)
