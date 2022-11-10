4cbcd05e1a7d2d2f0eec9946c1b688800899d808
murongxixi <murongxixi@gmail.com>
2022-11-11 03:52:33 +0800

    update

--- codes/lcs.py
+++ codes/lcs.py
@@ -1,132 +1,38 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-
-import time
-from bisect import bisect_left
-
-import matplotlib.pyplot as plt
 import numpy as np
 
 
 def restore_lcs(b, x, i, j, lcs):
     if i == -1 or j == -1:
         return
-    if b[i, j] == -1:
+    if b[i][j] == '↖':
         restore_lcs(b, x, i-1, j-1, lcs)
         lcs.append(x[i])
-    elif b[i, j] == -2:
+    elif b[i][j] == '↑':
         restore_lcs(b, x, i-1, j, lcs)
     else:
         restore_lcs(b, x, i, j-1, lcs)
 
 
-trial = 8
-time_cost = np.zeros([trial, 3])
-k = 0
-for n in [2**v for v in range(8, 8+trial)]:
+X = ['A', 'B', 'C', 'B', 'D', 'A', 'B']
+Y = ['B', 'D', 'C', 'A', 'B', 'A']
+m, n = len(X), len(Y)
+b = [[0 for i in range(m)] for j in range(n)]
+c = [[0 for i in range(m+1)] for j in range(n+1)]
 
-    np.random.seed(1)
-    x = np.random.permutation(n) + 1  # 生成第一个排列
-    # print("X: %s" % x)
-
-    np.random.seed(2)
-    y = np.random.permutation(n) + 1  # 生成第二个排列
-    # print("Y: %s" % y)
-
-    b = np.zeros([n, n])
-    c = np.zeros([n+1, n+1])
-
-    t = time.time()
-    for i in range(1, n+1):
-        for j in range(1, n+1):
-            if x[i-1] == y[j-1]:
-                c[i, j] = c[i-1, j-1] + 1  # 如果当前两个子串的最后一个字符相同
-                b[i-1, j-1] = -1
-            elif c[i-1, j] >= c[i, j-1]:
-                c[i, j] = c[i-1, j]
-                b[i-1, j-1] = -2
-            else:
-                c[i, j] = c[i, j-1]
-                b[i-1, j-1] = -3
-    time_cost[k, 0] = time.time() - t
-
-    # lcs = []
-    # restore_lcs(b, x, n-1, n-1, lcs)
-    # print('LCS: %s' % lcs)
-
-    dict = {key: value for key, value in zip(x, range(1, n+1))}  # X -> [1:n]
-    # print("sigma: %s" % dict)
-
-    # x2 = [dict[i] for i in x]
-    # print("X: %s -> %s" % (x, x2))
-
-    y2 = [dict[i] for i in y]
-    # print("Y: %s -> %s" % (y, y2))
-
-    # lcs2 = [dict[i] for i in lcs]
-    # print('LCS: %s -> %s' % (lcs, lcs2))
-
-    x = y2
-    # print('X: %s' % x)
-
-    d = np.ones(n, dtype=np.int32)
-    p = -1 * np.ones(n, dtype=np.int32)
-    t = time.time()
-    for i in range(1, n):
-        for j in range(i):
-            if y2[j] < y2[i] and d[i] < d[j] + 1:
-                d[i] = d[j] + 1
-                p[i] = j
-    time_cost[k, 1] = time.time() - t
-
-    # print('d: %s' % d)
-
-    # lis_len_index = np.argmax(d)
-    # lis = [y2[lis_len_index]]
-    # pos = p[lis_len_index]
-    # while pos != -1:
-    #     lis.append(y2[pos])
-    #     pos = p[pos]
-    # lis.reverse()
-    # print('LIS: %s' % lis)
-
-    e = []
-    p = -1 * np.ones(n, dtype=np.int32)
-    t = time.time()
-    for i in range(n):
-        j = bisect_left(e, x[i])
-        if j == len(e):
-            if j > 0:
-                p[i] = e[-1]
-            e.append(x[i])
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if X[i-1] == Y[j-1]:           # 如果当前两个子串的最后一个字符相同
+            c[i][j] = c[i-1][j-1] + 1
+            b[i-1][j-1] = '↖'
+        elif c[i-1][j] >= c[i][j-1]:
+            c[i][j] = c[i-1][j]
+            b[i-1][j-1] = '↑'
         else:
-            e[j] = x[i]
-            p[i] = e[j-1]
-        # print("iteration %d: e = %s" % (i+1, e))
-    time_cost[k, 2] = time.time() - t
+            c[i][j] = c[i][j-1]
+            b[i-1][j-1] = '←'
 
-    # dict = {key: value for key, value in zip(x, range(n))}
-    # lis = []
-    # pre = e[-1]
-    # for i in range(len(e)):
-    #     lis.append(pre)
-    #     pre = p[dict[pre]]
-
-    # lis.reverse()
-    # print('LIS: %s' % lis)
-    k = k + 1
-
-print(time_cost)
-
-with plt.style.context('Solarize_Light2'):
-    x = [v for v in range(8, 8+trial)]
-    plt.plot(x, np.log2(time_cost[:, 0]), label="LCS & DP")
-    plt.plot(x, np.log2(time_cost[:, 1]), label="LIS & DP")
-    plt.plot(x, np.log2(time_cost[:, 2]), label="LIS & Greedy")
-    plt.xlabel('n: power of 2')
-    plt.ylabel('time')
-    plt.legend()
-    plt.grid(color='#93a1a1', linestyle='-.', linewidth=0.7)
-
-    plt.savefig('lcs.svg', transparent=True)
-    plt.show()
+lcs = []
+restore_lcs(b, X, n-1, n-1, lcs)
+print('LCS: %s' % lcs)
+print(c)
+print(b)
--- codes/matrix-chain.py
+++ codes/matrix-chain.py
@@ -1,19 +1,18 @@
 import sys
-import numpy as np
 
 
 def matrix_chain(p):
     for l in range(2, n+1):        # 子问题长度 l = 2 -> n
         for i in range(1, n-l+2):  # 从第i个矩阵开始
             j = i + l - 1          # 到第j个矩阵结束
-            m[i, j] = sys.maxsize  # 初始化为无穷大
+            m[i][j] = sys.maxsize  # 初始化为无穷大
             for k in range(i, j):  # 遍历最优分开位置
-                cost = m[i, k] + m[k+1, j] + p[i-1] * p[k] * p[j]
-                if cost < m[i, j]:
-                    m[i, j] = cost
-                    s[i, j] = k
+                cost = m[i][k] + m[k+1][j] + p[i-1] * p[k] * p[j]
+                if cost < m[i][j]:
+                    m[i][j] = cost
+                    s[i][j] = k
                 k = k + 1
-    return m[1, n]
+    return m[1][n]
 
 
 def print_sol(s, i, j):
@@ -21,20 +20,20 @@ def print_sol(s, i, j):
         print("A%d" % (i), end='')
     else:
         print("(", end='')
-        print_sol(s, i, s[i, j])
-        print_sol(s, s[i, j]+1, j)
+        print_sol(s, i, s[i][j])
+        print_sol(s, s[i][j]+1, j)
         print(")", end='')
 
 
 p = [30, 35, 15, 5, 10, 20, 25]
 n = len(p) - 1  # 矩阵个数
-m = np.zeros((n+1, n+1), dtype=np.int64)
-s = np.zeros((n+1, n+1), dtype=np.int64)
+m = [[0 for i in range(n+1)] for j in range(n+1)]
+s = [[0 for i in range(n+1)] for j in range(n+1)]
 print(matrix_chain(p))
-print(m[1:, 1:])
-print(s[1:, 1:])
+print(m)
+print(s)
 print_sol(s, 1, n)
-----------------------------------------
+------------------------------------------
 15125
 m = [[    0 15750  7875  9375 11875 15125]
      [    0     0  2625  4375  7125 10500]
