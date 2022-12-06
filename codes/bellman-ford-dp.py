87b59ecee6fefc7ccb15f69cf0f40d8766ff10a0
murongxixi <murongxixi@gmail.com>
2022-12-06 18:29:32 +0800

    update

--- codes/bellman-ford-dp.py
+++ codes/bellman-ford-dp.py
@@ -1,14 +1,19 @@
-def bellman_ford(s):
+def bellman_ford(g, s):
     d, p = dict(), dict()
     for v in g:  # 距离初始化为无穷大 前驱初始化为空
         d[v], p[v] = float("inf"), None
     d[s] = 0  # 源点到自己的最短路径长度为零
 
-    for _ in range(len(g) - 1):  # 遍历|V|-1次
+    print("初始：", d)
+
+    for i in range(len(g) - 1):  # 遍历|V|-1次
+        
         for u in g:
             for v in g[u]:  # 内部的二重for循环遍历所有边
                 if d[v] > d[u] + g[u][v]:           # 松弛
                     d[v], p[v] = d[u] + g[u][v], u  # 更新当前最短距离和前驱
+
+        print("初始：", d)
 
     for u in g:
         for v in g[u]:
@@ -17,17 +22,27 @@ def bellman_ford(s):
     return d, p
 
 
-g = {
-    "s": {"t": 6, "y": 7},           # w(s,t) = 6, w(s,y) = 7
-    "t": {"x": 5, "z": -4, "y": 8},  # w(t,x) = 5, w(t,z) = -4, w(t,y) = 8
-    "y": {"z": 9, "x": -3},          # w(y,z) = 9, w(y,x) = -3
-    "z": {"x": 7, "s": 2},           # w(z,x) = 7, w(z,s) = 2
-    "x": {"t": -2},                  # w(x,t) = -2
+g1 = {               # 用集合表示有向图g 元素为字典
+    "s": {"x": 1},  # w(s,x) = 1
+    "x": {"y": 1},  # w(x,y) = 1
+    "y": {"z": 1},  # w(y,z) = 1
+    "z": {"t": 1},  # w(z,t) = 1
+    "t": {}
 }
 
-d, p = bellman_ford(s="s")
+g2 = {               # 用集合表示有向图g 元素为字典
+    "t": {},
+    "z": {"t": 1},  # w(z,t) = 1
+    "y": {"z": 1},  # w(y,z) = 1
+    "x": {"y": 1},  # w(x,y) = 1
+    "s": {"x": 1},  # w(s,x) = 1
+}
+
+d, p = bellman_ford(g1, s="s")
 print(d)
-print(p)
+
+d, p = bellman_ford(g2, s="s")
+print(d)
 # ---------------------------------------------------
 # {'s': 0, 't': 2, 'y': 7, 'z': -2, 'x': 4}
 # {'s': None, 't': 'x', 'y': 's', 'z': 't', 'x': 'y'}
