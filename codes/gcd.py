54a3d5bd5ac9a037070edb9c62ce373db9f3c14f
murongxixi <murongxixi@gmail.com>
2023-10-23 18:32:03 +0800

    update

--- codes/gcd.py
+++ codes/gcd.py
@@ -1,4 +1,4 @@
-def gcd1(a, b):
+def gcd(a, b):
     for i in range(1, min(a, b) + 1):        # 最大公约数不会大于两者中的较小者
         if ((a % i == 0) and (b % i == 0)):  # 同时整除即为公约数
             gcd = i
@@ -12,14 +12,14 @@ def coef(a, b):
             return x, y
 
 
-def gcd2(a, b):  # 辗转相除
+def Euclidean(a, b):  # 辗转相除
     a, b = max(a, b), min(a, b)
     while a % b:
         a, b = b, a % b
     return b
 
 
-def gcd3(a, b):  # 更相减损
+def gxjs(a, b):  # 更相减损
     while True:
         if a > b:
             a = a - b
@@ -29,23 +29,30 @@ def gcd3(a, b):  # 更相减损
             return b
 
 
-def gcd4(a, b):  # 改进的更相减损
+def gxjs_mod(a, b):  # 改进的更相减损
     if a == b:
-        return b
+        return a, 1, 0
     while True:
         if not (a & 1) and not (b & 1):  # 均为偶 gcd(a,b) = 2 * gcd(a/2, b/2)
-            return gcd4(a >> 1, b >> 1) << 1
+            d, x, y = gxjs_mod(a >> 1, b >> 1)
+            return d << 1, x, y
         elif not (a & 1) and (b & 1):    # a偶 b奇 gcd(a,b) = gcd(a/2, b)
-            return gcd4(a >> 1, b)
+            d, x, y = gxjs_mod(a >> 1, b)
+            return d, x << 1, y
         elif (a & 1) and not (b & 1):    # a奇 b偶 gcd(a,b) = gcd(a, b/2)
-            return gcd4(a, b >> 1)
+            d, x, y = gxjs_mod(a, b >> 1)
+            return d, x, y << 1
         else:                            # 均为奇 更相减损 gcd(a,b) = gcd(a-b, b)
-            a, b = max(a, b), min(a, b)
-            return gcd4(a-b, b)
+            if a > b:
+                d, x, y = gxjs_mod(a-b, b)
+                return d, x, y-x
+            else:
+                d, x, y = gxjs_mod(a, b-a)
+                return d, x-y, y
 
 
-print(gcd1(2022, 1566))
-print(coef(8, 9))
-print(gcd2(2022, 1566))
-print(gcd3(2022, 1566))
-print(gcd4(2022, 1566))
+print(gcd(2024, 1024))
+print(coef(2023, 1024))
+print(Euclidean(2024, 1024))
+print(gxjs(2024, 1024))
+print(gxjs_mod(8, 20))
