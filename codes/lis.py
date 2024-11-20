a6861f37e7b2b79379a4082240bd5021baa61e71
murongxixi <murongxixi@gmail.com>
2024-11-20 15:28:29 +0800

    update

--- codes/lis.py
+++ codes/lis.py
@@ -5,7 +5,7 @@ import numpy as np
 # np.random.seed(0)
 
 
-def lis_dp():
+def lis_dp(X, n):
     d, b = [1] * n, [-1] * n
     for i in range(1, n):
         for j in range(i):
@@ -15,30 +15,30 @@ def lis_dp():
     return d, b
 
 
-def restore_lis_dp(index, LIS):
+def restore_lis_dp(X, b, index, LIS):
     if index == -1:
         return
-    restore_lis_dp(b[index], LIS)
+    restore_lis_dp(X, b, b[index], LIS)
     LIS.append(X[index])
 
 
-def lis_dp_plus():
+def lis_dp_plus(X, n):
     e = []
     b = [-1] * n  # b[i]记录i在LIS中的前一个元素的值
     for i in range(n):
         j = bisect_left(e, X[i])  # 在e中对X[i]进行二分查找
-        if j == len(e):           # X[i] > e[-1]
+        if j == len(e):  # X[i] > e[-1]
             if j > 0:
                 b[i] = e[-1]
-            e.append(X[i])        # 将其接在e后面表示找到了更长的递增子序列
-        else:                     # e[j-1] < X[i] < e[j]
+            e.append(X[i])  # 将其接在e后面表示找到了更长的递增子序列
+        else:  # e[j-1] < X[i] < e[j]
             if j > 0:
-                b[i] = e[j-1]
-            e[j] = X[i]           # 将e[j]改为X[i] 可以改进现有长度为j的递增子序列
+                b[i] = e[j - 1]
+            e[j] = X[i]  # 将e[j]改为X[i] 可以改进现有长度为j的递增子序列
     return e, b
 
 
-def restore_lis_dp_plus(e, b):
+def restore_lis_dp_plus(X, n, e, b):
     dict = {key: value for key, value in zip(X, range(n))}  # 倒排索引字典 X[i]: i
     pre = e[-1]  # e的最后一个元素是LIS的最后一个元素
     LIS = []
@@ -55,19 +55,19 @@ def restore_lis_dp_plus_aux(dict, b, pre, LIS):
 
 n = 10
 X = np.random.permutation(n)
-print('X =', X)
+print("X =", X)
 
-d, b = lis_dp()
+d, b = lis_dp(X, n)
 # print('d: %s' % d)
 # print('b: %s' % b)
 
 LIS = []
-restore_lis_dp(np.argmax(d), LIS)
-print('LIS =', LIS)
+restore_lis_dp(X, b, np.argmax(d), LIS)
+print("LIS =", LIS)
 
-e, b = lis_dp_plus()
+e, b = lis_dp_plus(X, n)
 # print('e: %s' % e)
 # print('b: %s' % b)
 
-LIS = restore_lis_dp_plus(e, b)
-print('LIS =', LIS)
+LIS = restore_lis_dp_plus(X, n, e, b)
+print("LIS =", LIS)
