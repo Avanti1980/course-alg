5d88cd33a478f8ea0d52957d917ce61452eab392
murongxixi <murongxixi@gmail.com>
2024-12-12 12:25:20 +0800

    update

--- codes/ford-fulkerson.py
+++ codes/ford-fulkerson.py
@@ -1,7 +1,63 @@
-c = {                                # 容量
-    "s": {"t": 6, "y": 7},           # w(s,t) = 6, w(s,y) = 7
-    "t": {"x": 5, "z": -4, "y": 8},  # w(t,x) = 5, w(t,z) = -4, w(t,y) = 8
-    "y": {"z": 9, "x": -3},          # w(y,z) = 9, w(y,x) = -3
-    "z": {"x": 7, "s": 2},           # w(z,x) = 7, w(z,s) = 2
-    "x": {"t": -2},                  # w(x,t) = -2
+def dfs(graph, node, visited, pre):
+    if node not in visited:
+        visited.append(node)
+        for neighbor in graph[node].keys():
+            dfs(graph, neighbor, visited, pre)
+            pre[neighbor] = node
+
+def augmenting_path(c):
+    pre = dict()
+    dfs(c, "s", [], pre)
+    print(pre)
+    
+def ford_fulkerson(c, f):
+
+    # 计算残存网络
+    c_f = dict()
+    for u in c:
+        c_f[u] = dict()
+    for u in c:
+        for v in c[u]:
+            if f[u][v] == c[u][v]:
+                c_f[v][u] = c[u][v]
+            elif f[u][v] == 0:
+                c_f[u][v] = c[u][v]
+            else:
+                c_f[u][v] = c[u][v] - f[u][v]
+                c_f[v][u] = f[u][v]
+
+    print(c_f)
+
+    augmenting_path(c_f)
+
+c = {                            # 容量
+    "s": {"v1": 16, "v2": 13},   # c(s,v1) = 16, c(s,v2) = 13
+    "v1": {"v3": 12},            # c(v1,v3) = 12
+    "v2": {"v1": 4, "v4": 14},   # c(v2,v1) = 4, c(v2,v4) = 14
+    "v3": {"v2": 9, "t": 20},    # c(v3,v2) = 9, c(v3,t) = 20
+    "v4": {"v3": 7, "t": 4},     # c(v4,v3) = 7, c(v4,t) = 4
+    "t": {}
 }
+
+f = {
+    "s": {"v1": 0, "v2": 0},
+    "v1": {"v3": 0},
+    "v2": {"v1": 0, "v4": 0},
+    "v3": {"v2": 0, "t": 0},
+    "v4": {"v3": 0, "t": 0},
+}
+
+f = {
+    "s": {"v1": 4, "v2": 4},
+    "v1": {"v3": 8},
+    "v2": {"v1": 4, "v4": 4},
+    "v3": {"v2": 4, "t": 4},
+    "v4": {"v3": 0, "t": 4},
+}
+
+ford_fulkerson(c, f)
+
+
+
+
+
