5738b774b32105524ba0fa5c3f6083d28bd7dd8f
murongxixi <murongxixi@gmail.com>
2024-12-16 09:41:46 +0800

    update

--- codes/ford-fulkerson.py
+++ codes/ford-fulkerson.py
@@ -1,133 +1,245 @@
-def ford_fulkerson(c):
+from queue import PriorityQueue
+import itertools
 
-    # 初始化流值为零
-    f = dict()
+
+def initialize_flow(c):  # 零初始化
+    f = {}
     for u in c:
-        f[u] = dict()
+        f[u] = {}
     for u in c:
         for v in c[u]:
             f[u][v] = 0
+    return f
 
-    i = 1
+
+def res_net(c, f):                             # 计算残存网络
+    c_f = {}
+    for u in c:
+        c_f[u] = {}
+    for u in c:
+        for v in c[u]:
+            if f[u][v] == c[u][v]:             # 满流量
+                c_f[v][u] = c[u][v]
+            elif f[u][v] == 0:                 # 无流量
+                c_f[u][v] = c[u][v]
+            else:
+                c_f[u][v] = c[u][v] - f[u][v]  # 正向边容量等于剩余容量
+                c_f[v][u] = f[u][v]            # 反向边容量等于当前流量
+    return c_f
+
+
+def dfs_rec(c_f, pre, visited, u):  # 深度优先 递归
+    if u == "t":
+        return
+    for v in c_f[u].keys():         # 残存网络中 u -> v
+        if v not in visited:
+            visited.append(v)
+            pre[v] = u
+            dfs_rec(c_f, pre, visited, v)
+
+
+def xxfs_iter(c_f, n):
+    pre, visited, l = {}, ["s"], ["s"]  # 前驱表 访问标记 用于做搜索的数据结构
+    while l:
+        u = l.pop(n)                    # 弹出第n个元素
+        for v in c_f[u].keys():         # 残存网络中 u -> v
+            if v not in visited:
+                visited.append(v)
+                l.append(v)
+                pre[v] = u
+                if v == "t":
+                    return pre
+    return pre
+
+
+def longest_path(c_f):  # 穷举检查 带环有向图中寻找最长简单路径是NP难的
+    nodes = []          # 存储全部中间结点
+    for u in c_f:
+        if u != "s" and u != "t":
+            nodes.append(u)
+    pre = {}
+    for i in range(len(nodes), 0, -1):
+        combination = list(itertools.combinations(nodes, i))  # 按结点数从多到少遍历幂集
+        for comb in combination:
+            permutation = list(itertools.permutations(comb))  # 对于某个结点子集 生成全排列
+            for perm in permutation:
+                find_path = True
+                if perm[0] not in c_f["s"].keys():   # 如果没有 s -> 第一个点
+                    find_path = False
+                if "t" not in c_f[perm[-1]].keys():  # 如果没有 最后一个点 -> t
+                    find_path = False
+                for j in range(1, len(perm)):
+                    if perm[j] not in c_f[perm[j - 1]].keys():
+                        find_path = False
+                        break
+                if find_path:
+                    pre["t"] = perm[-1]
+                    for j in range(len(perm) - 1, 0, -1):
+                        pre[perm[j]] = perm[j - 1]
+                    pre[perm[0]] = "s"
+                    return pre
+    return pre
+
+
+def max_res_cap(c_f):    # 类似于最小生成树的Prim算法
+    pre, weight, in_tree, pq = {}, {}, [], PriorityQueue()
+    for v in c_f:
+        weight[v] = -1   # 记录还没加入树中的点与树中结点的边的最大容量
+
+    pq.put([None, "s"])
+    while not pq.empty():
+        _, u = pq.get()  # 获取队首元素点u
+        if u not in in_tree:
+            in_tree.append(u)
+            for v in c_f[u].keys():          # 残存网络中 u -> v
+                if v not in in_tree and weight[v] < c_f[u][v]:
+                    weight[v] = c_f[u][v]
+                    pq.put([-weight[v], v])  # 最小优先队列找最大容量 故取反加入
+                    pre[v] = u
+    return pre
+
+
+def increase_flow(c_f, f, pre):
+    v, path = "t", []
+    while True:                      # 根据前驱表生成增广路径
+        path.append((pre[v], v))
+        v = pre[v]
+        if v == "s":
+            break
+    path.reverse()
+    print("增广路径:", path)
+
+    res_cap_edge = []                # 存储增广路径上边的容量
+    for u, v in path:
+        res_cap_edge.append(c_f[u][v])
+        res_cap = min(res_cap_edge)  # 确定残存容量
+    print("残存容量:", res_cap)
+
+    for u, v in path:
+        if v in f[u].keys():
+            f[u][v] += res_cap       # 正向边流量增大
+        else:
+            f[v][u] -= res_cap       # 反向边流量减小
+
+
+def calculate_flow(f):
+    value = 0
+    for v in f["s"].keys():
+        value += f["s"][v]
+    return value
+
+
+def ford_fulkerson(c, mode):
+
+    f, i = initialize_flow(c), 1  # 初始化流值为零
+
     while True:
-
         print("第%d轮" % i)
         print("当前的流:", f)
 
-        # 计算残存网络
-        c_f = dict()
-        for u in c:
-            c_f[u] = dict()
-        for u in c:
-            for v in c[u]:
-                if f[u][v] == c[u][v]:             # 满流量
-                    c_f[v][u] = c[u][v]
-                elif f[u][v] == 0:                 # 无流量
-                    c_f[u][v] = c[u][v]
-                else:
-                    c_f[u][v] = c[u][v] - f[u][v]  # 正向边容量等于剩余容量
-                    c_f[v][u] = f[u][v]            # 反向边容量等于当前流量
+        c_f = res_net(c, f)  # 计算残存网络
         print("残存网络:", c_f)
 
-        # 寻找增广路径
-        pre, visited, stack = dict(), [], ["s"]    # 前驱表 访问标记 用于DFS的栈
-        while stack:
-            node = stack.pop()
-            visited.append(node)
-            for neighbor in c_f[node].keys():      # 残存网络中node的邻接点
-                if neighbor not in visited:
-                    stack.append(neighbor)
-                    pre[neighbor] = node
-                    if neighbor == "t":
-                        break
+        match mode:
+            case 1:  # 深度优先 递归
+                pre = {}
+                dfs_rec(c_f, pre, ["s"], "s")
+            case 2:  # 深度优先 循环 栈
+                pre = xxfs_iter(c_f, -1)
+            case 3:  # 广度优先 循环 队列 or 最少边数
+                pre = xxfs_iter(c_f, 0)
+            case 4:  # 最多边数
+                pre = longest_path(c_f)
+            case 5:  # 最大残存容量
+                pre = max_res_cap(c_f)
+            case _:
+                print("mode取值必须是{1,2,3,4,5}!")
+                return
 
-        if "t" not in pre.keys():  # 若汇点、源点已不再连通
-            max_f = 0
-            for v in f["s"].keys():
-                max_f += f["s"][v]
-            print("最大流值为%d" % max_f)
+        if "t" not in pre.keys():   # 若汇点、源点已不再连通
+            print("最大流值为%d" % calculate_flow(f))
             return
-
-        key, path = "t", []
-        while True:                # 根据DFS的前驱表生成增广路径
-            path.append((pre[key], key))
-            key = pre[key]
-            if key == "s":
-                break
-        path.reverse()
-        print("增广路径:", path)
-
-        # 确定残存容量
-        c_f_p = float("inf")
-        for u, v in path:
-            if c_f_p > c_f[u][v]:
-                c_f_p = c_f[u][v]
-        print("残存容量:", c_f_p)
-
-        # 修改流值
-        for u, v in path:
-            if v in f[u].keys():
-                f[u][v] += c_f_p
-            else:
-                f[v][u] -= c_f_p
+        else:
+            increase_flow(c_f, f, pre)
 
         i += 1
 
 
-c = {                            # 容量
-    "s": {"v1": 16, "v2": 13},   # c(s,v1) = 16, c(s,v2) = 13          v1 -- v3
-    "v1": {"v3": 12},            # c(v1,v3) = 12                     / |   / | \
-    "v2": {"v1": 4, "v4": 14},   # c(v2,v1) = 4, c(v2,v4) = 14     s   |  /  |  t
-    "v3": {"v2": 9, "t": 20},    # c(v3,v2) = 9, c(v3,t) = 20        \ | /   | /
-    "v4": {"v3": 7, "t": 4},     # c(v4,v3) = 7, c(v4,t) = 4          v2 -- v4
+c = {                           # 容量
+    "s": {"v1": 16, "v2": 13},  # c(s,v1) = 16, c(s,v2) = 13        v1 -- v3
+    "v1": {"v3": 12},           # c(v1,v3) = 12                   / |   / | \
+    "v2": {"v1": 4, "v4": 14},  # c(v2,v1) = 4, c(v2,v4) = 14   s   |  /  |  t
+    "v3": {"v2": 9, "t": 20},   # c(v3,v2) = 9, c(v3,t) = 20      \ | /   | /
+    "v4": {"v3": 7, "t": 4},    # c(v4,v3) = 7, c(v4,t) = 4        v2 -- v4
     "t": {}
 }
-ford_fulkerson(c)
-# ----------------------------------------------------------
+ford_fulkerson(c, mode=5)
+# -----------------------------------------------------
 # 第1轮
 # 当前的流: {'s': {'v1': 0, 'v2': 0}, 'v1': {'v3': 0}, 'v2': {'v1': 0, 'v4': 0}, 'v3': {'v2': 0, 't': 0}, 'v4': {'v3': 0, 't': 0}, 't': {}}
 # 残存网络: {'s': {'v1': 16, 'v2': 13}, 'v1': {'v3': 12}, 'v2': {'v1': 4, 'v4': 14}, 'v3': {'v2': 9, 't': 20}, 'v4': {'v3': 7, 't': 4}, 't': {}}
-# 增广路径: [('s', 'v2'), ('v2', 'v4'), ('v4', 't')]
-# 残存容量: 4
+# 增广路径: [('s', 'v1'), ('v1', 'v3'), ('v3', 't')]
+# 残存容量: 12
 # 第2轮
-# 当前的流: {'s': {'v1': 0, 'v2': 4}, 'v1': {'v3': 0}, 'v2': {'v1': 0, 'v4': 4}, 'v3': {'v2': 0, 't': 0}, 'v4': {'v3': 0, 't': 4}, 't': {}}
-# 残存网络: {'s': {'v1': 16, 'v2': 9}, 'v1': {'v3': 12}, 'v2': {'s': 4, 'v1': 4, 'v4': 10}, 'v3': {'v2': 9, 't': 20}, 'v4': {'v2': 4, 'v3': 7}, 't': {'v4': 4}}
+# 当前的流: {'s': {'v1': 12, 'v2': 0}, 'v1': {'v3': 12}, 'v2': {'v1': 0, 'v4': 0}, 'v3': {'v2': 0, 't': 12}, 'v4': {'v3': 0, 't': 0}, 't': {}}
+# 残存网络: {'s': {'v1': 4, 'v2': 13}, 'v1': {'s': 12}, 'v2': {'v1': 4, 'v4': 14}, 'v3': {'v1': 12, 'v2': 9, 't': 8}, 'v4': {'v3': 7, 't': 4}, 't': {'v3': 12}}
 # 增广路径: [('s', 'v2'), ('v2', 'v4'), ('v4', 'v3'), ('v3', 't')]
 # 残存容量: 7
 # 第3轮
-# 当前的流: {'s': {'v1': 0, 'v2': 11}, 'v1': {'v3': 0}, 'v2': {'v1': 0, 'v4': 11}, 'v3': {'v2': 0, 't': 7}, 'v4': {'v3': 7, 't': 4}, 't': {}}
-# 残存网络: {'s': {'v1': 16, 'v2': 2}, 'v1': {'v3': 12}, 'v2': {'s': 11, 'v1': 4, 'v4': 3}, 'v3': {'v2': 9, 't': 13, 'v4': 7}, 'v4': {'v2': 11}, 't': {'v3': 7, 'v4': 4}}
-# 增广路径: [('s', 'v2'), ('v2', 'v1'), ('v1', 'v3'), ('v3', 't')]
-# 残存容量: 2
+# 当前的流: {'s': {'v1': 12, 'v2': 7}, 'v1': {'v3': 12}, 'v2': {'v1': 0, 'v4': 7}, 'v3': {'v2': 0, 't': 19}, 'v4': {'v3': 7, 't': 0}, 't': {}}
+# 残存网络: {'s': {'v1': 4, 'v2': 6}, 'v1': {'s': 12}, 'v2': {'s': 7, 'v1': 4, 'v4': 7}, 'v3': {'v1': 12, 'v2': 9, 't': 1, 'v4': 7}, 'v4': {'v2': 7, 't': 4}, 't': {'v3': 19}}
+# 增广路径: [('s', 'v2'), ('v2', 'v4'), ('v4', 't')]
+# 残存容量: 4
 # 第4轮
-# 当前的流: {'s': {'v1': 0, 'v2': 13}, 'v1': {'v3': 2}, 'v2': {'v1': 2, 'v4': 11}, 'v3': {'v2': 0, 't': 9}, 'v4': {'v3': 7, 't': 4}, 't': {}}
-# 残存网络: {'s': {'v1': 16}, 'v1': {'v3': 10, 'v2': 2}, 'v2': {'s': 13, 'v1': 2, 'v4': 3}, 'v3': {'v1': 2, 'v2': 9, 't': 11, 'v4': 7}, 'v4': {'v2': 11}, 't': {'v3': 9, 'v4': 4}}
-# 增广路径: [('s', 'v1'), ('v1', 'v3'), ('v3', 't')]
-# 残存容量: 10
-# 第5轮
-# 当前的流: {'s': {'v1': 10, 'v2': 13}, 'v1': {'v3': 12}, 'v2': {'v1': 2, 'v4': 11}, 'v3': {'v2': 0, 't': 19}, 'v4': {'v3': 7, 't': 4}, 't': {}}
-# 残存网络: {'s': {'v1': 6}, 'v1': {'s': 10, 'v2': 2}, 'v2': {'s': 13, 'v1': 2, 'v4': 3}, 'v3': {'v1': 12, 'v2': 9, 't': 1, 'v4': 7}, 'v4': {'v2': 11}, 't': {'v3': 19, 'v4': 4}}
+# 当前的流: {'s': {'v1': 12, 'v2': 11}, 'v1': {'v3': 12}, 'v2': {'v1': 0, 'v4': 11}, 'v3': {'v2': 0, 't': 19}, 'v4': {'v3': 7, 't': 4}, 't': {}}
+# 残存网络: {'s': {'v1': 4, 'v2': 2}, 'v1': {'s': 12}, 'v2': {'s': 11, 'v1': 4, 'v4': 3}, 'v3': {'v1': 12, 'v2': 9, 't': 1, 'v4': 7}, 'v4': {'v2': 11}, 't': {'v3': 19, 'v4': 4}}
 # 最大流值为23
 
-c = {                            # 容量                             v
-    "s": {"u": 100, "v": 100},   # c(s,u) = 100, c(s,v) = 100    / | \
-    "u": {"v": 1, "t": 100},     # c(u,v) = 1, c(u,t) = 100     s  |  t
-    "v": {"t": 100},             # c(v,t) = 100                  \ | /
-    "t": {}                      #                                 u
+c = {                           # 容量                             v
+    "s": {"u": 100, "v": 100},  # c(s,u) = 100, c(s,v) = 100    / | \
+    "u": {"v": 1, "t": 100},    # c(u,v) = 1, c(u,t) = 100     s  |  t
+    "v": {"t": 100},            # c(v,t) = 100                  \ | /
+    "t": {}                     # #                               u
 }
-ford_fulkerson(c)
-# ----------------------------------------------------------
+ford_fulkerson(c, mode=4)
+# ------------------------------------
 # 第1轮
 # 当前的流: {'s': {'u': 0, 'v': 0}, 'u': {'v': 0, 't': 0}, 'v': {'t': 0}, 't': {}}
 # 残存网络: {'s': {'u': 100, 'v': 100}, 'u': {'v': 1, 't': 100}, 'v': {'t': 100}, 't': {}}
-# 增广路径: [('s', 'v'), ('v', 't')]
-# 残存容量: 100
+# 增广路径: [('s', 'u'), ('u', 'v'), ('v', 't')]
+# 残存容量: 1
 # 第2轮
-# 当前的流: {'s': {'u': 0, 'v': 100}, 'u': {'v': 0, 't': 0}, 'v': {'t': 100}, 't': {}}
-# 残存网络: {'s': {'u': 100}, 'u': {'v': 1, 't': 100}, 'v': {'s': 100}, 't': {'v': 100}}
-# 增广路径: [('s', 'u'), ('u', 't')]
-# 残存容量: 100
+# 当前的流: {'s': {'u': 1, 'v': 0}, 'u': {'v': 1, 't': 0}, 'v': {'t': 1}, 't': {}}
+# 残存网络: {'s': {'u': 99, 'v': 100}, 'u': {'s': 1, 't': 100}, 'v': {'u': 1, 't': 99}, 't': {'v': 1}}
+# 增广路径: [('s', 'v'), ('v', 'u'), ('u', 't')]
+# 残存容量: 1
 # 第3轮
+# 当前的流: {'s': {'u': 1, 'v': 1}, 'u': {'v': 0, 't': 1}, 'v': {'t': 1}, 't': {}}
+# 残存网络: {'s': {'u': 99, 'v': 99}, 'u': {'s': 1, 'v': 1, 't': 99}, 'v': {'s': 1, 't': 99}, 't': {'u': 1, 'v': 1}}
+# 增广路径: [('s', 'u'), ('u', 'v'), ('v', 't')]
+# 残存容量: 1
+# 第4轮
+# 当前的流: {'s': {'u': 2, 'v': 1}, 'u': {'v': 1, 't': 1}, 'v': {'t': 2}, 't': {}}
+# 残存网络: {'s': {'u': 98, 'v': 99}, 'u': {'s': 2, 't': 99}, 'v': {'s': 1, 'u': 1, 't': 98}, 't': {'u': 1, 'v': 2}}
+# 增广路径: [('s', 'v'), ('v', 'u'), ('u', 't')]
+# 残存容量: 1
+# 第5轮
+# 当前的流: {'s': {'u': 2, 'v': 2}, 'u': {'v': 0, 't': 2}, 'v': {'t': 2}, 't': {}}
+# 残存网络: {'s': {'u': 98, 'v': 98}, 'u': {'s': 2, 'v': 1, 't': 98}, 'v': {'s': 2, 't': 98}, 't': {'u': 2, 'v': 2}}
+# 增广路径: [('s', 'u'), ('u', 'v'), ('v', 't')]
+# 残存容量: 1
+# ...
+# 第201轮
 # 当前的流: {'s': {'u': 100, 'v': 100}, 'u': {'v': 0, 't': 100}, 'v': {'t': 100}, 't': {}}
 # 残存网络: {'s': {}, 'u': {'s': 100, 'v': 1}, 'v': {'s': 100}, 't': {'u': 100, 'v': 100}}
 # 最大流值为200
+
+c = {                               # #         s
+    "s": {"a": 1, "b": 1, "d": 1},  # #      ╱|  ╲
+    "a": {"t": 1},                  # #   ╱  |     ╲
+    "b": {"a": 1, "c": 1},          # # a -- b -- c -- d
+    "c": {"t": 1},                  # #   ╲     /  ╱
+    "d": {"c": 1, "t": 1},          # #     ╲ / ╱
+    "t": {}                         # #       t
+}
+ford_fulkerson(c, mode=1)
--- slides/test.md
+++ slides/test.md
@@ -23,3 +23,33 @@ presentation:
 @import "../plugin/chalkboard/style.css"
 @import "../plugin/menu/menu.js"
 
+<!-- slide data-notes="" -->
+
+##### 最大边不相交路径数
+
+---
+
+任给有向图，求任意两点间的最大边不相交路径数
+
+下图$s \rightsquigarrow t$有 3 条边不相交的路径，是否为最大？
+
+<div class="top4">
+    <img src="../tikz/max-flow-edge-disjoint1.svg" width=30%>
+    <img src="../tikz/max-flow-edge-disjoint2.svg" width=30% class="fragment left10" data-fragment-index="1">
+</div>
+
+<p class="fragment" data-fragment-index="1">归约为单位容量的流网络的最大流问题</p>
+
+<!-- slide vertical=true data-notes="" -->
+
+##### 最大边不相交路径数
+
+---
+
+一方面，对任意$k$条边不相交的路径，每条上面可运输单位流量，得到流值为$k$的流，因此最大边不相交路径数$\le$最大流值
+
+另一方面，由于边的容量均为整数，因此最大流在各条边上的流值也为整数，非$0$即$1$，每条边要么满流量、要么零流量；根据流分解定理，最大流可分解为若干条$s \rightsquigarrow t$的路径，每条路径上的流量为$1$，因此这些路径不会有公共边，故最大流值$\le$最大边不相交路径数
+
+利用 James Orlin 算法，时间复杂度为$O(|\Vcal| |\Ecal|)$
+
+事实上，切割$(\{s\}, \Vcal \setminus \{s\})$的容量最多为$|\Vcal| - 1$，因此最大流值不超过$|\Vcal| - 1$，朴素的 Ford - Fulkerson 算法也有$O(|\Vcal| |\Ecal|)$的时间复杂度
